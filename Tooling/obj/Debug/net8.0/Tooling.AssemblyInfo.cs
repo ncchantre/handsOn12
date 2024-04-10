1f6e1392043a501abd3b1e23ea304e08670f6807
ncchantre <nicholascchantre@outlook.com>
2024-04-09 21:41:54 -0400

    Debugging

--- Tooling/obj/Debug/net8.0/Tooling.AssemblyInfo.cs
+++ Tooling/obj/Debug/net8.0/Tooling.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tooling")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1b43e262d588451ddeb414b028cb3b0e903105c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c9f96e540bd2de276ad71d86ff67b892a51612a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tooling")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tooling")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
